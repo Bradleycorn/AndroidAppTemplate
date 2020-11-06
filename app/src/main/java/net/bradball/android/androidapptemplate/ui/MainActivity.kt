5437abf685a6285edcf624f58f1511e3d4ad71e9
brad.ball <brad.ball@twinspires.com>
2020-11-06 13:25:07 -0500

    setup a basic (empty) app with Jetpack Compose

--- app/src/main/java/net/bradball/android/androidapptemplate/ui/MainActivity.kt
+++ app/src/main/java/net/bradball/android/androidapptemplate/ui/MainActivity.kt
@@ -3,27 +3,46 @@ package net.bradball.android.androidapptemplate.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
-import net.bradball.android.androidapptemplate.R
+import net.bradball.android.androidapptemplate.ui.theme.MyApplicationTheme
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setupToolbar()
+        setContent {
+            MyApplicationTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    AppScreen()
+                }
+            }
+        }
     }
 
-    private fun setupToolbar() {
-        val toolbar = findViewById<Toolbar>(R.id.app_bar)
-        val navController = findNavController(R.id.nav_host_fragment)
-        setSupportActionBar(toolbar)
-        NavigationUI.setupWithNavController(toolbar, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+    @Preview
+    @Composable
+    fun AppScreen() {
+        Row(horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+                .fillMaxWidth()) {
+
+            Text(text = "AppScreen",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h6)
+        }
     }
+
+
+
 }
--- app/src/main/java/net/bradball/android/androidapptemplate/ui/rootFragment/RootViewModel.kt
+++
@@ -1,8 +0,0 @@
-package net.bradball.android.androidapptemplate.ui.rootFragment
-
-import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
-
-class RootViewModel @ViewModelInject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
-}
